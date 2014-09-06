4ac9e4e6f714dd594473d0ca366ed84665be05d2
Enrique Hurtarte Juarez <enrique.hurtarte@gmail.com>
2014-09-06 15:38:59 -0600

    clase de php

---
+++ php/miprimerpagina.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>mi primera página PHP</title>
+    </head>
+    <body>
+        <br><br>
+        <?php
+            echo "Parte de php<br>";
+            
+            for($i=0;$i<10;$i++)
+            {
+                echo "Linea ".$i."<br>";
+            }
+        ?>
+        
+    </body>
+    
+</html>
+
+
+
